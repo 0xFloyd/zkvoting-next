2dfb796eb0b220d604e20ab8acf3423bb8a38ddb
0xFloyd <32312309+0xFloyd@users.noreply.github.com>
2023-03-08 14:38:33 -0500

    add screenshot

--- hardhat/contracts/ZKVoting.sol
+++ hardhat/contracts/ZKVoting.sol
@@ -68,7 +68,7 @@ contract ZKVoting {
         emit AddLeaf(input[2], input[3], input[1]);
     }
 
-    function proveLeaf(
+    function proveMembership(
         uint[2] memory a,
         uint[2][2] memory b,
         uint[2] memory c,
